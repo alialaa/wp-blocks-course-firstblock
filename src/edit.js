f9fbcf0fe7fbf8da6fc470f6c5629d439d2c7904
Ali Alaa <hello@alialaa.com>
2021-06-25 10:09:13 +0200

    Update edit.js

--- src/edit.js
+++ src/edit.js
@@ -1,7 +1,7 @@
 import { useBlockProps } from "@wordpress/block-editor";
 import "./editor.scss";
 
-export default function save() {
+export default function edit() {
     const blockProps = useBlockProps();
     return <p {...blockProps}>Edit JSX</p>;
 }
